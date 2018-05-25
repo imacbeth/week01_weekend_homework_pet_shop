72fc6e71eadc4f8ab2c606c2e3364cea02f6207d
Iona Macbeth <ionalmacbeth@gmail.com>
2018-05-25 15:12:21 +0100

    initial commit

---
+++ pet_shop.rb
@@ -0,0 +1,35 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+     pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold)
+  pet_shop[:admin][:pets_sold] += sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+
+def pets_by_breed(pet_shop, breeds)
+  all_breeds = []
+  for breed_type in pet_shop[:pets]
+    if breed_type[:breed] == breeds
+      all_breeds.push(breed_type)
+    end
+  end
+  return all_breeds
+end
